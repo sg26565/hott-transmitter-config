7459e693635ba5a19f311446c5f77375a88798d3
Winfried Bruegmann <winfried.bruegmann@gmail.com>
2013-04-09 17:10:28 +0000

    initial SWT UI

--- HoTTGUI/src/main/java/gde/mdl/ui/ModelTreeDialog.java
+++ HoTTGUI/src/main/java/gde/mdl/ui/ModelTreeDialog.java
@@ -3,11 +3,7 @@ package gde.mdl.ui;
 import gde.mdl.ui.composites.BaseConfiguration;
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.swt.layout.FormAttachment;
-import org.eclipse.swt.layout.FormData;
-import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
@@ -18,18 +14,6 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Tree;
 import org.eclipse.swt.widgets.TreeItem;
 
-/**
-* This code was edited or generated using CloudGarden's Jigloo
-* SWT/Swing GUI Builder, which is free for non-commercial
-* use. If Jigloo is being used commercially (ie, by a corporation,
-* company or business for any purpose whatever) then you
-* should purchase a license for each developer using Jigloo.
-* Please visit www.cloudgarden.com for details.
-* Use of Jigloo implies acceptance of these licensing terms.
-* A COMMERCIAL LICENSE HAS NOT BEEN PURCHASED FOR
-* THIS MACHINE, SO JIGLOO OR THIS CODE CANNOT BE USED
-* LEGALLY FOR ANY CORPORATE OR COMMERCIAL PURPOSE.
-*/
 public class ModelTreeDialog extends org.eclipse.swt.widgets.Dialog {
 
 	private Shell dialogShell;
