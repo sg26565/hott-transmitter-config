e65e1e2626d7ca2be2bc04a0c6c048c5e23e9300
Winfried Bruegmann <winfried.bruegmann@gmail.com>
2013-04-09 18:34:48 +0000

    initial SWT UI

--- HoTTGUI/src/main/java/gde/mdl/ui/composites/BaseConfiguration.java
+++ HoTTGUI/src/main/java/gde/mdl/ui/composites/BaseConfiguration.java
@@ -10,6 +10,19 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Text;
 
 
+
+/**
+* This code was edited or generated using CloudGarden's Jigloo
+* SWT/Swing GUI Builder, which is free for non-commercial
+* use. If Jigloo is being used commercially (ie, by a corporation,
+* company or business for any purpose whatever) then you
+* should purchase a license for each developer using Jigloo.
+* Please visit www.cloudgarden.com for details.
+* Use of Jigloo implies acceptance of these licensing terms.
+* A COMMERCIAL LICENSE HAS NOT BEEN PURCHASED FOR
+* THIS MACHINE, SO JIGLOO OR THIS CODE CANNOT BE USED
+* LEGALLY FOR ANY CORPORATE OR COMMERCIAL PURPOSE.
+*/
 public class BaseConfiguration extends ScrolledComposite {
 
 	private CLabel						infoLabel;
@@ -19,7 +32,6 @@ public class BaseConfiguration extends ScrolledComposite {
 	private CLabel						clockResetLabel;
 	private CCombo						powerWarningCombo;
 	private CLabel						txLabel1, txLabel2, txLabel3, txLabel4;
-	private Text							controlModeText;
 	private CCombo						autoResetClockCombo;
 	private CCombo						autoTrimCombo;
 	private CCombo						txModuleCombo;
@@ -50,14 +62,14 @@ public class BaseConfiguration extends ScrolledComposite {
 		baseComposite = new Composite(this, SWT.NONE);
 		this.setContent(baseComposite);
 		GridLayout composite1Layout1 = new GridLayout();
-		composite1Layout1.numColumns = 5;
+		composite1Layout1.numColumns = 6;
 		baseComposite.setSize(500, 300);
 		baseComposite.setLayout(composite1Layout1);
 		if (header != null && header.length() > 0){
 			headerLabel = new CLabel(baseComposite, SWT.NONE);
 			headerLabel.setText(header);
 			GridData gridData = new GridData();
-			gridData.horizontalSpan = 5;
+			gridData.horizontalSpan = 6;
 			gridData.widthHint = 175;
 			gridData.heightHint = 22;
 			headerLabel.setLayoutData(gridData);
@@ -69,6 +81,7 @@ public class BaseConfiguration extends ScrolledComposite {
 			GridData gridData = new GridData();
 			gridData.widthHint = 175;
 			gridData.heightHint = 22;
+			gridData.horizontalSpan = 2;
 			modellNameLabel.setLayoutData(gridData);
 
 		}
@@ -86,6 +99,7 @@ public class BaseConfiguration extends ScrolledComposite {
 			GridData infoLabelLData = new GridData();
 			infoLabelLData.widthHint = 175;
 			infoLabelLData.heightHint = 22;
+			infoLabelLData.horizontalSpan = 2;
 			infoLabel.setLayoutData(infoLabelLData);
 			infoLabel.setText("Information");
 		}
@@ -101,6 +115,7 @@ public class BaseConfiguration extends ScrolledComposite {
 			GridData stickModeLabelLData = new GridData();
 			stickModeLabelLData.widthHint = 175;
 			stickModeLabelLData.heightHint = 22;
+			stickModeLabelLData.horizontalSpan = 2;
 			stickModeLabel.setLayoutData(stickModeLabelLData);
 			stickModeLabel.setText("Steueranordnung");
 		}
@@ -116,7 +131,7 @@ public class BaseConfiguration extends ScrolledComposite {
 		{
 			modulTypeLabel = new CLabel(baseComposite, SWT.NONE);
 			GridData modulTypeLabelLData = new GridData();
-			modulTypeLabelLData.widthHint = 175;
+			modulTypeLabelLData.widthHint = 105;
 			modulTypeLabelLData.heightHint = 22;
 			modulTypeLabel.setLayoutData(modulTypeLabelLData);
 			modulTypeLabel.setText("Modul");
@@ -124,19 +139,10 @@ public class BaseConfiguration extends ScrolledComposite {
 		{
 			txModuleCombo = new CCombo(baseComposite, SWT.BORDER);
 			GridData txModuleComboLData = new GridData();
-			txModuleComboLData.widthHint = 100;
+			txModuleComboLData.widthHint = 60;
 			txModuleComboLData.heightHint = 17;
-			txModuleComboLData.horizontalSpan = 4;
 			txModuleCombo.setLayoutData(txModuleComboLData);
 			txModuleCombo.setText("HoTT");
-		}
-		{
-			receiverLabel = new CLabel(baseComposite, SWT.NONE);
-			GridData receiverLabelLData = new GridData();
-			receiverLabelLData.widthHint = 175;
-			receiverLabelLData.heightHint = 22;
-			receiverLabel.setLayoutData(receiverLabelLData);
-			receiverLabel.setText("Empfänger Ausgang");
 		}
 		{
 			txLabel1 = new CLabel(baseComposite, SWT.CENTER | SWT.EMBEDDED | SWT.BORDER);
@@ -171,27 +177,70 @@ public class BaseConfiguration extends ScrolledComposite {
 			txLabel4.setText("----");
 		}
 		{
+			receiverLabel = new CLabel(baseComposite, SWT.NONE);
+			GridData receiverLabelLData = new GridData();
+			receiverLabelLData.widthHint = 175;
+			receiverLabelLData.heightHint = 22;
+			receiverLabelLData.horizontalSpan = 2;
+			receiverLabel.setLayoutData(receiverLabelLData);
+			receiverLabel.setText("Empfänger Ausgang");
+		}
+		{
+			txLabel1 = new CLabel(baseComposite, SWT.CENTER | SWT.EMBEDDED | SWT.BORDER);
+			GridData txLabel1LData = new GridData();
+			txLabel1LData.widthHint = 29;
+			txLabel1LData.heightHint = 22;
+			txLabel1.setLayoutData(txLabel1LData);
+			txLabel1.setText("E08");
+		}
+		{
+			txLabel2 = new CLabel(baseComposite, SWT.CENTER | SWT.EMBEDDED | SWT.BORDER);
+			GridData txLabel2LData = new GridData();
+			txLabel2LData.widthHint = 29;
+			txLabel2LData.heightHint = 22;
+			txLabel2.setLayoutData(txLabel2LData);
+			txLabel2.setText("n/v");
+		}
+		{
+			txLabel3 = new CLabel(baseComposite, SWT.CENTER | SWT.EMBEDDED | SWT.BORDER);
+			GridData txLabel3LData1 = new GridData();
+			txLabel3LData1.widthHint = 29;
+			txLabel3LData1.heightHint = 22;
+			txLabel3.setLayoutData(txLabel3LData1);
+			txLabel3.setText("n/v");
+		}
+		{
+			txLabel4 = new CLabel(baseComposite, SWT.CENTER | SWT.EMBEDDED | SWT.BORDER);
+			GridData txLabel4LData1 = new GridData();
+			txLabel4LData1.widthHint = 29;
+			txLabel4LData1.heightHint = 22;
+			txLabel4.setLayoutData(txLabel4LData1);
+			txLabel4.setText("n/v");
+		}
+		{
 			dscOutputLabel = new CLabel(baseComposite, SWT.NONE);
 			GridData dscOutputLabelLData = new GridData();
 			dscOutputLabelLData.widthHint = 175;
 			dscOutputLabelLData.heightHint = 22;
+			dscOutputLabelLData.horizontalSpan = 2;
 			dscOutputLabel.setLayoutData(dscOutputLabelLData);
 			dscOutputLabel.setText("DSC-Ausgang");
 		}
 		{
-			controlModeText = new Text(baseComposite, SWT.CENTER | SWT.BORDER);
+			controlModeCombo = new CCombo(baseComposite, SWT.CENTER | SWT.BORDER);
 			GridData controlModeTextLData = new GridData();
-			controlModeTextLData.widthHint = 21;
-			controlModeTextLData.heightHint = 16;
+			controlModeTextLData.widthHint = 60;
+			controlModeTextLData.heightHint = 17;
 			controlModeTextLData.horizontalSpan = 4;
-			controlModeText.setLayoutData(controlModeTextLData);
-			controlModeText.setText("1");
+			controlModeCombo.setLayoutData(controlModeTextLData);
+			controlModeCombo.setText("PPM10");
 		}
 		{
 			powerWarningLabel = new CLabel(baseComposite, SWT.NONE);
 			GridData powerWarningLabelLData = new GridData();
 			powerWarningLabelLData.widthHint = 175;
 			powerWarningLabelLData.heightHint = 22;
+			powerWarningLabelLData.horizontalSpan = 2;
 			powerWarningLabel.setLayoutData(powerWarningLabelLData);
 			powerWarningLabel.setText("Einschaltwarnung");
 		}
@@ -199,16 +248,17 @@ public class BaseConfiguration extends ScrolledComposite {
 			powerWarningCombo = new CCombo(baseComposite, SWT.BORDER);
 			GridData powerWarningComboLData = new GridData();
 			powerWarningComboLData.horizontalSpan = 4;
-			powerWarningComboLData.widthHint = 150;
+			powerWarningComboLData.widthHint = 60;
 			powerWarningComboLData.heightHint = 17;
 			powerWarningCombo.setLayoutData(powerWarningComboLData);
-			powerWarningCombo.setText("undefined");
+			powerWarningCombo.setText("G1");
 		}
 		{
 			autoTrimLabel = new CLabel(baseComposite, SWT.NONE);
 			GridData autoTrimLabelLData = new GridData();
 			autoTrimLabelLData.widthHint = 175;
 			autoTrimLabelLData.heightHint = 22;
+			autoTrimLabelLData.horizontalSpan = 2;
 			autoTrimLabel.setLayoutData(autoTrimLabelLData);
 			autoTrimLabel.setText("Auto-Trimm");
 		}
@@ -216,7 +266,7 @@ public class BaseConfiguration extends ScrolledComposite {
 			autoTrimCombo = new CCombo(baseComposite, SWT.BORDER);
 			GridData autoTrimComboLData = new GridData();
 			autoTrimComboLData.horizontalSpan = 4;
-			autoTrimComboLData.widthHint = 58;
+			autoTrimComboLData.widthHint = 60;
 			autoTrimComboLData.heightHint = 17;
 			autoTrimCombo.setLayoutData(autoTrimComboLData);
 			autoTrimCombo.setText("ON");
@@ -226,6 +276,7 @@ public class BaseConfiguration extends ScrolledComposite {
 			GridData clockResetLabelLData = new GridData();
 			clockResetLabelLData.widthHint = 175;
 			clockResetLabelLData.heightHint = 22;
+			clockResetLabelLData.horizontalSpan = 2;
 			clockResetLabel.setLayoutData(clockResetLabelLData);
 			clockResetLabel.setText("Auto rücksetzen Uhr");
 		}
@@ -233,7 +284,7 @@ public class BaseConfiguration extends ScrolledComposite {
 			autoResetClockCombo = new CCombo(baseComposite, SWT.BORDER);
 			GridData autoResetClockComboLData = new GridData();
 			autoResetClockComboLData.horizontalSpan = 4;
-			autoResetClockComboLData.widthHint = 58;
+			autoResetClockComboLData.widthHint = 60;
 			autoResetClockComboLData.heightHint = 17;
 			autoResetClockCombo.setLayoutData(autoResetClockComboLData);
 			autoResetClockCombo.setText("ON");
