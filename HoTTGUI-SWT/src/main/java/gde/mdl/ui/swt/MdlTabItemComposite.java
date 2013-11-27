67c6332cc87549c99268d7825eb2810d387ab1c4
Oliver Treichel <oli@treichels.de>
2013-11-27 20:13:17 +0000

    SWT based user interface

---
+++ HoTTGUI-SWT/src/main/java/gde/mdl/ui/swt/MdlTabItemComposite.java
@@ -0,0 +1,266 @@
+package gde.mdl.ui.swt;
+
+import gde.mdl.ui.swing.Launcher;
+import gde.model.BaseModel;
+import gde.report.Report;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.jar.Manifest;
+import java.util.logging.Level;
+import java.util.prefs.Preferences;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.KeyListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.FontData;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
+import org.eclipse.swt.widgets.MessageBox;
+
+public class MdlTabItemComposite extends Composite {
+  private static final Preferences PREFS = Preferences.userNodeForPackage(MdlTabItemComposite.class);
+
+  private Browser                  browser;
+  private final Font               font;
+  private Button                   loadMdlButton;
+  private MenuItem                 loadMdlMenuItem;
+  private Label                    mdlVersionLabel;
+  private BaseModel                model = null;
+  private MenuItem                 reloadMenuItem;
+  private Button                   saveMdlButton;
+  private MenuItem                 saveMdlMenuItem;
+
+  public MdlTabItemComposite(final Composite parent) {
+    super(parent, SWT.NONE);
+    final GridLayout tabItemCompositeLayout = new GridLayout();
+    tabItemCompositeLayout.numColumns = 3;
+    setLayout(tabItemCompositeLayout);
+    setBackground(new Color(Display.getDefault(), 250, 249, 211));
+    final FontData fontData = parent.getFont().getFontData()[0];
+    fontData.setHeight(fontData.getHeight() - 1);
+    font = SWTResourceManager.getFont(SwtMdlBrowser.WIDGET_FONT_NAME, SwtMdlBrowser.WIDGET_FONT_SIZE, SWT.NORMAL);
+    Report.setSuppressExceptions(false);
+    {
+      loadMdlButton = new Button(this, SWT.PUSH | SWT.CENTER);
+      final GridData loadMdlButtonLData = new GridData();
+      loadMdlButtonLData.widthHint = 180;
+      loadMdlButtonLData.heightHint = 26;
+      loadMdlButtonLData.verticalAlignment = GridData.BEGINNING;
+      loadMdlButtonLData.grabExcessHorizontalSpace = true;
+      loadMdlButtonLData.horizontalAlignment = GridData.CENTER;
+      loadMdlButton.setLayoutData(loadMdlButtonLData);
+      loadMdlButton.setFont(font);
+      loadMdlButton.setText("Load MDL"); //$NON-NLS-1$
+      loadMdlButton.addSelectionListener(new SelectionAdapter() {
+        @Override
+        public void widgetSelected(final SelectionEvent evt) {
+          MdlTabItem.log.log(Level.FINEST, "loadMdlButton.widgetSelected, event=" + evt); //$NON-NLS-1$
+          MdlTabItemComposite.this.load();
+        }
+      });
+    }
+    {
+      mdlVersionLabel = new Label(this, SWT.CENTER);
+      final GridData mdlVersionLabelLData = new GridData();
+      mdlVersionLabelLData.horizontalAlignment = GridData.CENTER;
+      // mdlVersionLabelLData.widthHint = 180; // no line wrap
+      mdlVersionLabelLData.heightHint = 26;
+      mdlVersionLabelLData.verticalAlignment = GridData.BEGINNING;
+      mdlVersionLabelLData.grabExcessHorizontalSpace = true;
+      mdlVersionLabel.setLayoutData(mdlVersionLabelLData);
+      mdlVersionLabel.setBackground(new Color(Display.getDefault(), 250, 249, 211));
+      mdlVersionLabel.setFont(font);
+      String version = "?"; //$NON-NLS-1$
+      try {
+        @SuppressWarnings("rawtypes")
+        final Class clazz = MdlTabItem.class;
+        final String className = clazz.getSimpleName() + ".class"; //$NON-NLS-1$
+        final String classPath = clazz.getResource(className).toString();
+        if (classPath.startsWith("jar")) { //$NON-NLS-1$
+          final String manifestPath = classPath.substring(0, classPath.lastIndexOf("!") + 1) + "/META-INF/MANIFEST.MF"; //$NON-NLS-1$ //$NON-NLS-2$
+          final Manifest manifest = new Manifest(new URL(manifestPath).openStream());
+          version = manifest.getMainAttributes().getValue("Implementation-Version") + "." + manifest.getMainAttributes().getValue("Implementation-Build");
+        }
+        System.setProperty(Launcher.PROGRAM_VERSION, version);
+        mdlVersionLabel.setText("Implementation-Version: " + version); //$NON-NLS-1$
+      } catch (final IOException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+    }
+    {
+      saveMdlButton = new Button(this, SWT.PUSH | SWT.CENTER);
+      final GridData saveMdlButtonLData = new GridData();
+      saveMdlButtonLData.horizontalAlignment = GridData.CENTER;
+      saveMdlButtonLData.widthHint = 180;
+      saveMdlButtonLData.heightHint = 26;
+      saveMdlButtonLData.verticalAlignment = GridData.BEGINNING;
+      saveMdlButtonLData.grabExcessHorizontalSpace = true;
+      saveMdlButton.setLayoutData(saveMdlButtonLData);
+      saveMdlButton.setFont(font);
+      saveMdlButton.setText("Save PDF/HTML/XML"); //$NON-NLS-1$
+      saveMdlButton.setEnabled(false);
+      saveMdlButton.addSelectionListener(new SelectionAdapter() {
+        @Override
+        public void widgetSelected(final SelectionEvent evt) {
+          MdlTabItem.log.log(Level.FINEST, "saveMdlButton.widgetSelected, event=" + evt); //$NON-NLS-1$
+          MdlTabItemComposite.this.save();
+        }
+      });
+    }
+    {
+      browser = new Browser(this, SWT.BORDER);
+      final GridData viewTextLData = new GridData();
+      viewTextLData.grabExcessHorizontalSpace = true;
+      viewTextLData.verticalAlignment = GridData.FILL;
+      viewTextLData.grabExcessVerticalSpace = true;
+      viewTextLData.horizontalSpan = 4;
+      viewTextLData.horizontalAlignment = GridData.FILL;
+      browser.setLayoutData(viewTextLData);
+
+      final Menu contextMenu = new Menu(browser);
+      loadMdlMenuItem = new MenuItem(contextMenu, SWT.NONE);
+      loadMdlMenuItem.setText("Load");
+      loadMdlMenuItem.addSelectionListener(new SelectionAdapter() {
+        @Override
+        public void widgetSelected(final SelectionEvent evt) {
+          MdlTabItem.log.log(Level.FINEST, "loadMdlMenuItem.widgetSelected, event=" + evt); //$NON-NLS-1$
+          MdlTabItemComposite.this.load();
+        }
+      });
+
+      saveMdlMenuItem = new MenuItem(contextMenu, SWT.NONE);
+      saveMdlMenuItem.setText("Save");
+      saveMdlMenuItem.setEnabled(false);
+      saveMdlMenuItem.addSelectionListener(new SelectionAdapter() {
+        @Override
+        public void widgetSelected(final SelectionEvent evt) {
+          MdlTabItem.log.log(Level.FINEST, "saveMdlMenuItem.widgetSelected, event=" + evt); //$NON-NLS-1$
+          MdlTabItemComposite.this.save();
+        }
+      });
+
+      new MenuItem(contextMenu, SWT.SEPARATOR);
+
+      reloadMenuItem = new MenuItem(contextMenu, SWT.NONE);
+      reloadMenuItem.setText("Reload");
+      reloadMenuItem.setEnabled(false);
+      reloadMenuItem.addSelectionListener(new SelectionAdapter() {
+        @Override
+        public void widgetSelected(final SelectionEvent evt) {
+          MdlTabItem.log.log(Level.FINEST, "reloadMenuItem.widgetSelected, event=" + evt); //$NON-NLS-1$
+          MdlTabItemComposite.this.updateView(false);
+        }
+      });
+
+      browser.setMenu(contextMenu);
+
+      browser.addKeyListener(new KeyListener() {
+        @Override
+        public void keyPressed(final KeyEvent evt) {
+          switch (evt.keyCode) {
+          case SWT.F5:
+            MdlTabItemComposite.this.updateView(false);
+            break;
+          }
+        }
+
+        @Override
+        public void keyReleased(final KeyEvent evt) {}
+      });
+    }
+  }
+
+  private void load() {
+    final FileDialog fd = new FileDialog(getParent().getShell(), SWT.SINGLE);
+    fd.setFilterExtensions(new String[] { "*.mdl" }); //$NON-NLS-1$
+    fd.setFilterNames(new String[] { "HoTT Transmitter Model Files (*.mdl)" }); //$NON-NLS-1$
+    fd.setFilterPath(PREFS.get("lastLoadDir", System.getProperty(Launcher.MDL_DIR)));
+
+    fd.open();
+    if (fd.getFileName().length() > 4) {
+      final File file = new File(fd.getFilterPath() + "/" + fd.getFileName()); //$NON-NLS-1$
+      if (file.exists() && file.canRead()) {
+        PREFS.put("lastLoadDir", file.getParentFile().getAbsolutePath());
+        try {
+          model = Report.getModel(file);
+          saveMdlButton.setEnabled(true);
+          saveMdlMenuItem.setEnabled(true);
+          reloadMenuItem.setEnabled(true);
+          updateView(false);
+        } catch (final Throwable t) {
+          final MessageBox mb = new MessageBox(getParent().getShell(), SWT.NONE);
+          mb.setText(t.getClass().getSimpleName());
+          mb.setMessage(t.getMessage() == null ? t.getClass().getSimpleName() : t.getMessage());
+          mb.open();
+          MdlTabItem.log.log(Level.SEVERE, t.getMessage(), t);
+        }
+      }
+    }
+  }
+
+  private void save() {
+    if (model != null) {
+      final FileDialog fileSaveDialog = new FileDialog(getParent().getShell(), SWT.PRIMARY_MODAL | SWT.SAVE);
+      fileSaveDialog.setText("save PDF"); //$NON-NLS-1$
+      fileSaveDialog.setFilterExtensions(new String[] { "*.pdf", "*.html", "*.xml" }); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+      fileSaveDialog.setFilterNames(new String[] {
+          "Portable Document Format (*.pdf)", "Hypertext Markup Language (*.html)", "Extensible Markup Language (*.xml)" }); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+      fileSaveDialog.setFilterPath(PREFS.get("lastSaveDir", PREFS.get("lastLoadDir", System.getProperty(Launcher.MDL_DIR))));
+      fileSaveDialog.setFileName(model.getModelName() + ".pdf");
+      fileSaveDialog.open();
+      final File file = new File(fileSaveDialog.getFilterPath() + "/" + fileSaveDialog.getFileName()); //$NON-NLS-1$
+      PREFS.put("lastSaveDir", file.getParentFile().getAbsolutePath());
+
+      try {
+        String data;
+        if (file.getName().endsWith(".xml")) { //$NON-NLS-1$
+          data = Report.generateXML(model);
+        } else {
+          data = Report.generateHTML(model);
+        }
+
+        if (file.getName().endsWith(".pdf")) { //$NON-NLS-1$
+          Report.savePDF(file, data);
+        } else {
+          Report.save(file, data);
+        }
+      } catch (final Throwable e) {
+        MdlTabItem.log.log(Level.WARNING, e.getMessage(), e);
+      }
+    }
+  }
+
+  private void updateView(final boolean isXML) {
+    if (model != null) {
+      String data;
+      try {
+        if (isXML) {
+          data = Report.generateXML(model);
+        } else {
+          data = Report.generateHTML(model);
+        }
+      } catch (final Exception e) {
+        data = e.getMessage();
+      }
+
+      browser.setText(data);
+    }
+
+    browser.setFocus();
+  }
+}
---
+++ HoTTGUI-SWT/src/main/java/gde/mdl/ui/swt/ModelTreeDialog.java
@@ -0,0 +1,178 @@
+package gde.mdl.ui.swt;
+
+import gde.mdl.ui.swt.composites.BaseConfiguration;
+import gde.mdl.ui.swt.composites.DualExpoConfiguration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeItem;
+
+
+public class ModelTreeDialog extends org.eclipse.swt.widgets.Dialog {
+
+	private Shell dialogShell;
+	private Menu				menu;
+	private MenuItem		saveItem;
+	private TreeItem treeItem2;
+	private TreeItem treeItem1;
+	private Composite editComposite;
+	private Composite mainComposite;
+	private Composite edit;
+	private Tree menuTree;
+	private MenuItem		loadItem;
+	private MenuItem		quitMenuItem;
+	
+	public enum Config { 
+		BASICS("Grundeinstellung"), DUALEXPO("Dualrate/Expotential");
+	private final String	value;
+
+	private Config(String v) {
+		this.value = v;
+	}
+
+	public String value() {
+		return this.value;
+	}
+	
+	public static Config fromValue(String v) {
+		for (Config c : Config.values()) {
+			if (c.value.equals(v)) {
+				return c;
+			}
+		}
+		throw new IllegalArgumentException(v);
+	}
+	
+	public static List<Config> getAsList() {
+		List<Config> sensors = new ArrayList<Config>();
+		for (Config sensor : Config.values()) {
+			sensors.add(sensor);
+		}
+		return sensors;
+	}
+};
+
+	/**
+	* Auto-generated main method to display this 
+	* org.eclipse.swt.widgets.Dialog inside a new Shell.
+	*/
+	public static void main(String[] args) {
+		try {
+			Display display = Display.getDefault();
+			Shell shell = new Shell(display);
+			ModelTreeDialog inst = new ModelTreeDialog(shell, SWT.NULL);
+			inst.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public ModelTreeDialog(Shell parent, int style) {
+		super(parent, style);
+	}
+
+	public void open() {
+		try {
+			Launcher.initSystemProperties();
+			Shell parent = getParent();
+			dialogShell = new Shell(parent, SWT.DIALOG_TRIM | SWT.APPLICATION_MODAL);
+			dialogShell.setText("Graupner/SJ HoTT MDL Explorer");
+			{
+				menu = new Menu(dialogShell, SWT.BAR | SWT.LEFT_TO_RIGHT);
+				dialogShell.setMenuBar(menu);
+				{
+					quitMenuItem = new MenuItem(menu, SWT.CASCADE);
+					quitMenuItem.setText("  Quit  ");
+				}
+				{
+					loadItem = new MenuItem(menu, SWT.CASCADE);
+					loadItem.setText(" load MDL ");
+				}
+				{
+					saveItem = new MenuItem(menu, SWT.PUSH);
+					saveItem.setText(" save MDL ");
+					saveItem.setEnabled(false);
+				}
+			}
+			FillLayout dialogShellLayout = new FillLayout(org.eclipse.swt.SWT.HORIZONTAL);
+			dialogShell.setLayout(dialogShellLayout);
+			{
+				mainComposite = new Composite(dialogShell, SWT.NONE);
+				GridLayout mainCompositeLayout = new GridLayout();
+				mainCompositeLayout.numColumns = 3;
+				mainComposite.setLayout(mainCompositeLayout);
+				{
+					GridData menuTreeLData = new GridData();
+					menuTreeLData.grabExcessVerticalSpace = true;
+					menuTreeLData.widthHint = 150;
+					menuTreeLData.verticalAlignment = GridData.FILL;
+					menuTree = new Tree(mainComposite, SWT.BORDER);
+					menuTree.setLayoutData(menuTreeLData);
+					menuTree.addSelectionListener(new SelectionAdapter() {
+						@Override
+						public void widgetSelected(SelectionEvent evt) {
+							System.out.println(evt);
+							if (edit != null) edit.dispose();
+							switch (Config.fromValue(((TreeItem)evt.item).getText())) {
+							case BASICS:
+							default:
+								edit = new BaseConfiguration(editComposite, Config.BASICS.value);
+								break;
+							case DUALEXPO:
+								edit = new DualExpoConfiguration(editComposite, Config.DUALEXPO.value);
+								break;
+							}
+							editComposite.layout();
+						}
+					});
+					{
+						treeItem1 = new TreeItem(menuTree, SWT.NONE);
+						treeItem1.setText(Config.BASICS.value);
+					}
+					{
+						treeItem2 = new TreeItem(menuTree, SWT.NONE);
+						treeItem2.setText(Config.DUALEXPO.value);
+					}
+
+				}
+				{
+					editComposite = new Composite(mainComposite, SWT.BORDER);
+					FormLayout editCompositeLayout = new FormLayout();
+					editComposite.setLayout(editCompositeLayout);
+					GridData editCompositeLData = new GridData();
+					editCompositeLData.horizontalSpan = 2;
+					editCompositeLData.grabExcessVerticalSpace = true;
+					editCompositeLData.widthHint = 400;
+					editCompositeLData.verticalAlignment = GridData.FILL;
+					editComposite.setLayoutData(editCompositeLData);
+				}
+			}
+			dialogShell.setLocation(getParent().toDisplay(100, 100));
+			dialogShell.layout();
+			dialogShell.pack();			
+			dialogShell.setSize(550, 500);
+			dialogShell.open();
+			Display display = dialogShell.getDisplay();
+			while (!dialogShell.isDisposed()) {
+				if (!display.readAndDispatch())
+					display.sleep();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
