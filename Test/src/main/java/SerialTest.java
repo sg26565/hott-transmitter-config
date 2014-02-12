107bf4e6c1c03d23a0a1bbb3c0359ae64d6021b4
Oliver Treichel <oli@treichels.de>
2014-02-12 19:57:29 +0100

    Signed-off-by: Oliver Treichel <oli@treichels.de>

--- Test/src/main/java/SerialTest.java
+++ Test/src/main/java/SerialTest.java
@@ -1,6 +1,4 @@
 import gde.model.serial.SerialPortDefaultImpl;
-import gde.model.serial.TxInfo;
-import gde.model.winged.WingedModel;
 import gnu.io.NoSuchPortException;
 import gnu.io.PortInUseException;
 import gnu.io.UnsupportedCommOperationException;
@@ -8,88 +6,38 @@ import gnu.io.UnsupportedCommOperationException;
 import java.io.IOException;
 import java.util.List;
 
-import de.treichels.hott.HoTTDecoder;
 import de.treichels.hott.HoTTTransmitter;
+import de.treichels.hott.internal.HoTTSerialPort;
+import de.treichels.hott.internal.PrepareListMdl;
+import de.treichels.hott.internal.QueryTxInfo;
+import de.treichels.hott.internal.ReadModelInformation;
+import de.treichels.hott.internal.ServoPositions;
 
 public class SerialTest {
-  private static void dump(final byte[] data) {
-    if (data == null) {
-      return;
-    }
-
-    final int len = data.length;
-    int addr = 0;
-    while (addr < len) {
-      System.out.printf("0x%04x:", addr);
-
-      for (int i = 0; i < 16 && addr < len; i++) {
-        final byte b = data[addr++];
-        final char c = (char) (b & 0xff);
-        if (c >= 0x20 && c <= 0x7e) {
-          System.out.printf(" %c ", c);
-
-        } else {
-          System.out.printf(" %02x", b);
-        }
-      }
-
-      System.out.println();
-    }
-  }
-
   public static void main(final String[] args) throws NoSuchPortException, UnsupportedCommOperationException, IOException, PortInUseException,
       InterruptedException {
     final List<String> ports = SerialPortDefaultImpl.getAvailablePorts();
 
     System.out.println(ports);
 
-    // HoTTSerialPort port = null;
-    // try {
-    // port = new HoTTSerialPort(ports.get(0));
-
-    // {
-    // final ServoPositions.Response response = (ServoPositions.Response)
-    // port.doCommand(new ServoPositions());
-    // for (int i = 0; i < 4; i++) {
-    // for (int j = 0; j < 8; j++) {
-    // System.out.printf(" %d", response.getPosition(i * 8 + j));
-    // }
-    //
-    // System.out.println();
-    // }
-    // }
-    //
-
     try {
       HoTTTransmitter.setSerialPortImpl(new SerialPortDefaultImpl(ports.get(0)));
-      final TxInfo info = HoTTTransmitter.getTxInfo();
 
-      System.out.println(info.getTransmitterType());
-      System.out.println(info.getAppVersion());
-      System.out.println(info.getMemoryVersion());
-      System.out.println(info.getYear());
-      System.out.println(info.getName());
-      System.out.println(info.getVendor());
+      // System.out.println(HoTTSerialPort.getInstance().doCommand(new
+      // FastModeStop()));
+      // System.out.println(HoTTSerialPort.getInstance().doCommand(new
+      // ClearScreen()));
+      // System.out.println(HoTTSerialPort.getInstance().doCommand(new
+      // ResetScreen()));
+      // System.out.println(HoTTSerialPort.getInstance().doCommand(new
+      // WriteScreen(0, "test")));
+      // System.out.println(HoTTSerialPort.getInstance().doCommand(new
+      // CloseScreen()));
 
-      final int[] positions = HoTTTransmitter.servoPositions();
-      for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 8; j++) {
-          System.out.printf(" %d", positions[i * 8 + j]);
-        }
-        System.out.println();
-      }
-
-      WingedModel model = (WingedModel) HoTTDecoder.decodeMemory(0);
-      System.out.printf("ModelName: %s, TransmitterId: 0x%x, MemoryVersion: %d\n", model.getModelName(), model.getTransmitterId(), model.getMemoryVersion());
-
-      model = (WingedModel) HoTTDecoder.decodeMemory(19);
-      System.out.printf("ModelName: %s, TransmitterId: 0x%x, MemoryVersion: %d\n", model.getModelName(), model.getTransmitterId(), model.getMemoryVersion());
-
-      HoTTTransmitter.writeScreen(new String[] { "a234567890123456789012345678901234567890", "b234567890123456789012345678901234567890",
-          "c234567890123456789012345678901234567890", "d234567890123456789012345678901234567890", "e234567890123456789012345678901234567890",
-          "f234567890123456789012345678901234567890" });
-      Thread.sleep(5000);
-      HoTTTransmitter.closeScreen();
+      System.out.println(HoTTSerialPort.getInstance().doCommand(new ReadModelInformation(0)));
+      System.out.println(HoTTSerialPort.getInstance().doCommand(new ServoPositions()));
+      System.out.println(HoTTSerialPort.getInstance().doCommand(new QueryTxInfo()));
+      System.out.println(HoTTSerialPort.getInstance().doCommand(new PrepareListMdl()));
     } finally {
       HoTTTransmitter.closeConnection();
     }
