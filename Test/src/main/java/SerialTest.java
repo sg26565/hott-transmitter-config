24c52726d067b7b75c0244d80867fa2014c1330b
Oliver Treichel <oli@treichels.de>
2014-02-12 21:32:59 +0100

    Signed-off-by: Oliver Treichel <oli@treichels.de>

--- Test/src/main/java/SerialTest.java
+++ Test/src/main/java/SerialTest.java
@@ -7,39 +7,51 @@ import java.io.IOException;
 import java.util.List;
 
 import de.treichels.hott.HoTTTransmitter;
+import de.treichels.hott.internal.AbstractCommand;
+import de.treichels.hott.internal.ClearScreen;
+import de.treichels.hott.internal.CloseScreen;
+import de.treichels.hott.internal.FastModeStop;
 import de.treichels.hott.internal.HoTTSerialPort;
 import de.treichels.hott.internal.PrepareListMdl;
+import de.treichels.hott.internal.QueryMdlNames;
 import de.treichels.hott.internal.QueryTxInfo;
 import de.treichels.hott.internal.ReadModelInformation;
+import de.treichels.hott.internal.ReadModelMemory;
+import de.treichels.hott.internal.ResetScreen;
 import de.treichels.hott.internal.ServoPositions;
+import de.treichels.hott.internal.WriteScreen;
 
 public class SerialTest {
   public static void main(final String[] args) throws NoSuchPortException, UnsupportedCommOperationException, IOException, PortInUseException,
       InterruptedException {
     final List<String> ports = SerialPortDefaultImpl.getAvailablePorts();
-
     System.out.println(ports);
 
     try {
       HoTTTransmitter.setSerialPortImpl(new SerialPortDefaultImpl(ports.get(0)));
 
-      // System.out.println(HoTTSerialPort.getInstance().doCommand(new
-      // FastModeStop()));
-      // System.out.println(HoTTSerialPort.getInstance().doCommand(new
-      // ClearScreen()));
-      // System.out.println(HoTTSerialPort.getInstance().doCommand(new
-      // ResetScreen()));
-      // System.out.println(HoTTSerialPort.getInstance().doCommand(new
-      // WriteScreen(0, "test")));
-      // System.out.println(HoTTSerialPort.getInstance().doCommand(new
-      // CloseScreen()));
+      test(new FastModeStop());
+      test(new ClearScreen());
+      test(new ResetScreen());
+      test(new WriteScreen(0, "test"));
+      test(new CloseScreen());
 
-      System.out.println(HoTTSerialPort.getInstance().doCommand(new ReadModelInformation(0)));
-      System.out.println(HoTTSerialPort.getInstance().doCommand(new ServoPositions()));
-      System.out.println(HoTTSerialPort.getInstance().doCommand(new QueryTxInfo()));
-      System.out.println(HoTTSerialPort.getInstance().doCommand(new PrepareListMdl()));
+      test(new ReadModelInformation(0));
+      test(new ReadModelMemory(4096));
+
+      test(new ServoPositions());
+
+      test(new QueryTxInfo());
+
+      test(new PrepareListMdl());
+      test(new QueryMdlNames());
     } finally {
       HoTTTransmitter.closeConnection();
     }
+  }
+
+  private static void test(final AbstractCommand cmd) throws IOException {
+    System.out.println(cmd);
+    System.out.println(HoTTSerialPort.getInstance().doCommand(cmd));
   }
 }
