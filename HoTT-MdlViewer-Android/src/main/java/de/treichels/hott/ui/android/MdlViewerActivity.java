8b02bf2d61f6605c1373ad88d967d6cdd2e75d30
Oliver Treichel <oli@treichels.de>
2014-02-16 14:42:15 +0100

    fixed merge
    Signed-off-by: Oliver Treichel <oli@treichels.de>
    (cherry picked from commit a5a95165c3608e4d276d1562a1be4987b0cb6dab)
    
    Conflicts:
    	Test/pom.xml

--- HoTT-MdlViewer-Android/src/main/java/de/treichels/hott/ui/android/MdlViewerActivity.java
+++ HoTT-MdlViewer-Android/src/main/java/de/treichels/hott/ui/android/MdlViewerActivity.java
@@ -6,20 +6,11 @@ import gde.model.enums.Section;
 import gde.report.ReportException;
 import gde.report.html.HTMLReport;
 
-<<<<<<< HEAD
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.net.MalformedURLException;
-=======
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintStream;
-import java.lang.Thread.UncaughtExceptionHandler;
->>>>>>> 79c76c652c9c873cce14b45b76bcd01813c19f34
 
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
@@ -31,13 +22,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
-<<<<<<< HEAD
 import android.os.Bundle;
-=======
-import android.os.Build;
-import android.os.Bundle;
-import android.os.Environment;
->>>>>>> 79c76c652c9c873cce14b45b76bcd01813c19f34
 import android.print.PrintManager;
 import android.provider.OpenableColumns;
 import android.support.v4.widget.DrawerLayout;
@@ -50,7 +35,6 @@ import android.widget.ListView;
 import android.widget.Toast;
 import de.treichels.hott.HoTTDecoder;
 
-<<<<<<< HEAD
 public class MdlViewerActivity extends Activity implements ListView.OnItemClickListener {
   private static final String MDL_MIME_TYPE      = "application/octet-stream";
   private static final String FILE_EXTENSION_MDL = ".mdl";
@@ -64,16 +48,6 @@ public class MdlViewerActivity extends Activity implements ListView.OnItemClickL
   private DrawerLayout        drawerLayout       = null;
   private ListView            drawer             = null;
   private String              loadUrl            = null;
-=======
-public class MdlViewerActivity extends Activity implements UncaughtExceptionHandler, ListView.OnItemClickListener {
-  private static final int READ_REQUEST_CODE    = 42;
-  private static boolean   NAVIGATION_SUPPORTED = Build.VERSION.SDK_INT >= 19;
-  private Uri              uri                  = null;
-  private BaseModel        model                = null;
-  private WebView          webView              = null;
-  private DrawerLayout     drawerLayout         = null;
-  private ListView         drawer               = null;
->>>>>>> 79c76c652c9c873cce14b45b76bcd01813c19f34
 
   /**
    * Read a MDL file and convert it to HTML.
@@ -82,11 +56,7 @@ public class MdlViewerActivity extends Activity implements UncaughtExceptionHand
    * @return
    */
   @SuppressLint("NewApi")
-<<<<<<< HEAD
   private void generateHtml() {
-=======
-  private String getHtml() {
->>>>>>> 79c76c652c9c873cce14b45b76bcd01813c19f34
     String fileName = null;
 
     if (ContentResolver.SCHEME_CONTENT.equals(uri.getScheme())) {
@@ -114,11 +84,7 @@ public class MdlViewerActivity extends Activity implements UncaughtExceptionHand
       throw new IllegalArgumentException(getResources().getString(R.string.msg_unsupported_uri, uri));
     }
 
-<<<<<<< HEAD
     if (fileName == null || !fileName.endsWith(FILE_EXTENSION_MDL)) {
-=======
-    if (fileName == null || !fileName.endsWith(".mdl")) {
->>>>>>> 79c76c652c9c873cce14b45b76bcd01813c19f34
       throw new IllegalArgumentException(getResources().getString(R.string.msg_invalid_file_name, fileName));
     }
 
@@ -143,11 +109,7 @@ public class MdlViewerActivity extends Activity implements UncaughtExceptionHand
     InputStream is = null;
     try {
       is = getContentResolver().openInputStream(uri);
-<<<<<<< HEAD
       model = HoTTDecoder.decodeStream(modelType, modelName, is);
-=======
-      model = HoTTDecoder.decode(modelType, modelName, is);
->>>>>>> 79c76c652c9c873cce14b45b76bcd01813c19f34
     } catch (final IOException e) {
       throw new RuntimeException(e);
     } finally {
@@ -160,7 +122,6 @@ public class MdlViewerActivity extends Activity implements UncaughtExceptionHand
       }
     }
 
-<<<<<<< HEAD
     String html;
     Writer writer = null;
     try {
@@ -171,17 +132,10 @@ public class MdlViewerActivity extends Activity implements UncaughtExceptionHand
       // write to cache file
       writer = new OutputStreamWriter(openFileOutput(CACHE_FILE_NAME, MODE_PRIVATE));
       writer.write(html);
-=======
-    // convert to HTML
-    try {
-      HTMLReport.setCurveImageGenerator(new AndroidCurveImageGenerator());
-      return HTMLReport.generateHTML(model);
->>>>>>> 79c76c652c9c873cce14b45b76bcd01813c19f34
     } catch (final ReportException e) {
       throw new RuntimeException(e);
     } catch (final IOException e) {
       throw new RuntimeException(e);
-<<<<<<< HEAD
     } finally {
       if (writer != null) {
         try {
@@ -190,8 +144,6 @@ public class MdlViewerActivity extends Activity implements UncaughtExceptionHand
           throw new RuntimeException(e);
         }
       }
-=======
->>>>>>> 79c76c652c9c873cce14b45b76bcd01813c19f34
     }
   }
 
@@ -215,62 +167,34 @@ public class MdlViewerActivity extends Activity implements UncaughtExceptionHand
    *          supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is
    *          null.</b>
    */
-<<<<<<< HEAD
-=======
-  @SuppressLint("SetJavaScriptEnabled")
->>>>>>> 79c76c652c9c873cce14b45b76bcd01813c19f34
   @Override
   public void onCreate(final Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
-<<<<<<< HEAD
     try {
       loadUrl = getFileStreamPath(CACHE_FILE_NAME).toURI().toURL().toString();
     } catch (final MalformedURLException e) {
       throw new RuntimeException(e);
     }
-=======
-    Thread.setDefaultUncaughtExceptionHandler(this);
->>>>>>> 79c76c652c9c873cce14b45b76bcd01813c19f34
 
     setContentView(R.layout.main);
     webView = (WebView) findViewById(R.id.webwiew);
     webView.getSettings().setBuiltInZoomControls(true);
 
-<<<<<<< HEAD
     drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
     drawer = (ListView) findViewById(R.id.drawer);
     drawer.setOnItemClickListener(this);
-=======
-    if (NAVIGATION_SUPPORTED) {
-      webView.getSettings().setJavaScriptEnabled(true);
-      registerForContextMenu(webView);
-
-      drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-      drawer = (ListView) findViewById(R.id.drawer);
-      drawer.setOnItemClickListener(this);
-    }
->>>>>>> 79c76c652c9c873cce14b45b76bcd01813c19f34
 
     // check if we were started via an intent
     final Intent intent = getIntent();
     if (intent != null && Intent.ACTION_VIEW.equals(intent.getAction())) {
       uri = intent.getData();
-<<<<<<< HEAD
-=======
-      updateUI(null);
->>>>>>> 79c76c652c9c873cce14b45b76bcd01813c19f34
     }
 
     // restore from saved state
     if (uri == null && savedInstanceState != null) {
-<<<<<<< HEAD
       if (savedInstanceState.containsKey(SAVED_STATE_URI)) {
         final String uriString = savedInstanceState.getString(SAVED_STATE_URI);
-=======
-      if (savedInstanceState.containsKey("uri")) {
-        final String uriString = savedInstanceState.getString("uri");
->>>>>>> 79c76c652c9c873cce14b45b76bcd01813c19f34
         if (uriString != null) {
           uri = Uri.parse(uriString);
         }
@@ -300,20 +224,12 @@ public class MdlViewerActivity extends Activity implements UncaughtExceptionHand
    * android.widget.AdapterView.OnItemClickListener#onItemClick(android.widget
    * .AdapterView, android.view.View, int, long)
    */
-<<<<<<< HEAD
-=======
-  @TargetApi(19)
->>>>>>> 79c76c652c9c873cce14b45b76bcd01813c19f34
   @Override
   public void onItemClick(final AdapterView<?> parent, final View view, final int position, final long id) {
     drawerLayout.closeDrawer(drawer);
 
     final Section section = Section.values()[(int) id];
-<<<<<<< HEAD
     webView.loadUrl(loadUrl + "#" + section.name());
-=======
-    webView.evaluateJavascript("location.hash='#" + section.name() + "'", null);
->>>>>>> 79c76c652c9c873cce14b45b76bcd01813c19f34
   }
 
   @Override
@@ -344,11 +260,7 @@ public class MdlViewerActivity extends Activity implements UncaughtExceptionHand
     super.onSaveInstanceState(outState);
 
     if (uri != null) {
-<<<<<<< HEAD
       outState.putString(SAVED_STATE_URI, uri.toString());
-=======
-      outState.putString("uri", uri.toString());
->>>>>>> 79c76c652c9c873cce14b45b76bcd01813c19f34
     }
   }
 
@@ -362,11 +274,7 @@ public class MdlViewerActivity extends Activity implements UncaughtExceptionHand
 
     intent = new Intent(Intent.ACTION_GET_CONTENT);
     intent.addCategory(Intent.CATEGORY_OPENABLE);
-<<<<<<< HEAD
     intent.setType(MDL_MIME_TYPE);
-=======
-    intent.setType("*/*");
->>>>>>> 79c76c652c9c873cce14b45b76bcd01813c19f34
 
     startActivityForResult(intent, READ_REQUEST_CODE);
   }
@@ -384,31 +292,6 @@ public class MdlViewerActivity extends Activity implements UncaughtExceptionHand
     printManager.print("HoTTMdlViewer - " + model.getModelName(), webView.createPrintDocumentAdapter(), null);
   }
 
-<<<<<<< HEAD
-=======
-  /*
-   * (non-Javadoc)
-   * 
-   * @see
-   * java.lang.Thread.UncaughtExceptionHandler#uncaughtException(java.lang.Thread
-   * , java.lang.Throwable)
-   */
-  @Override
-  public void uncaughtException(final Thread thread, final Throwable ex) {
-    final File downloadDir = Environment.getDownloadCacheDirectory();
-
-    if (downloadDir.exists() && downloadDir.isDirectory()) {
-      try {
-        final PrintStream ps = new PrintStream(new File(downloadDir, MdlViewerActivity.class.getName() + "_Exception.log"));
-        ex.printStackTrace(ps);
-        ps.close();
-      } catch (final FileNotFoundException e) {
-        // ignore
-      }
-    }
-  }
-
->>>>>>> 79c76c652c9c873cce14b45b76bcd01813c19f34
   /**
    * Update WebView in a background thread without blocking the UI thread.
    * 
@@ -429,30 +312,16 @@ public class MdlViewerActivity extends Activity implements UncaughtExceptionHand
       @Override
       protected Void doInBackground(final Void... params) {
         try {
-<<<<<<< HEAD
           generateHtml();
-=======
-          // decode and transform
-          final String html = getHtml();
->>>>>>> 79c76c652c9c873cce14b45b76bcd01813c19f34
 
           // access to View elements needs to be done in the UI thread
           runOnUiThread(new Runnable() {
             @Override
             public void run() {
-<<<<<<< HEAD
               webView.loadUrl(loadUrl);
               setTitle(model.getModelName());
 
               drawer.setAdapter(new SectionAdapter(MdlViewerActivity.this, model.getModelType(), model.getTransmitterType()));
-=======
-              webView.loadData(html, "text/html", "UTF-8");
-              setTitle(model.getModelName());
-
-              if (NAVIGATION_SUPPORTED) {
-                drawer.setAdapter(new SectionAdapter(MdlViewerActivity.this, model.getModelType(), model.getTransmitterType()));
-              }
->>>>>>> 79c76c652c9c873cce14b45b76bcd01813c19f34
 
               toast.cancel();
             }
@@ -472,11 +341,6 @@ public class MdlViewerActivity extends Activity implements UncaughtExceptionHand
               builder.create().show();
             }
           });
-<<<<<<< HEAD
-=======
-
-          uncaughtException(Thread.currentThread(), e);
->>>>>>> 79c76c652c9c873cce14b45b76bcd01813c19f34
         }
 
         return null;
