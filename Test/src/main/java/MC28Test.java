d8f56646127dda899005bfd2ef3673d64c0f15d8
Oliver Treichel <oli@treichels.de>
2017-05-04 20:46:29 +0200

    add more directories for test

--- Test/src/main/java/MC28Test.java
+++ Test/src/main/java/MC28Test.java
@@ -15,27 +15,36 @@ import gde.util.Util;
 
 public class MC28Test {
     public static void main(final String[] args) throws IOException, DocumentException {
-        final File dir = new File("C:/Users/olive/Google Drive/Graupner/models/mc28");
+        final File[] dirs = new File[] { new File("C:/Users/olive/Downloads"), new File("C:/Users/olive/Graupner/mc-32/Models-2.015"),
+                new File("C:/Users/olive/Graupner/mc-32/Models-1.017"), new File("C:/Users/olive/Graupner/mx-16/Models-1.807"),
+                new File("C:/Users/olive/Graupner/mx-16/Models-2.015"),
+                new File("C:/Users/olive/git/mdlviewer/hott-transmitter-config/HoTT-Report-HTML/src/test/resources/gde/report/html/models/mc20"),
+                new File("C:/Users/olive/git/mdlviewer/hott-transmitter-config/HoTT-Report-HTML/src/test/resources/gde/report/html/models/mc28"),
+                new File("C:/Users/olive/git/mdlviewer/hott-transmitter-config/HoTT-Report-HTML/src/test/resources/gde/report/html/models/mc32"),
+                new File("C:/Users/olive/git/mdlviewer/hott-transmitter-config/HoTT-Report-HTML/src/test/resources/gde/report/html/models/mx12"),
+                new File("C:/Users/olive/git/mdlviewer/hott-transmitter-config/HoTT-Report-HTML/src/test/resources/gde/report/html/models/mx16"),
+                new File("C:/Users/olive/git/mdlviewer/hott-transmitter-config/HoTT-Report-HTML/src/test/resources/gde/report/html/models/mx20"), };
 
-        for (final File file : dir.listFiles(f -> f.getName().endsWith(".mdl"))) {
-            final String baseName = FilenameUtils.getBaseName(file.getName());
-            System.out.println(baseName);
+        for (final File dir : dirs)
+            for (final File file : dir.listFiles(f -> f.getName().endsWith(".mdl"))) {
+                final String baseName = FilenameUtils.getBaseName(file.getName());
+                System.out.println(file.getAbsolutePath());
 
-            final byte[] data = Files.readAllBytes(file.toPath());
-            final String dump = Util.dumpData(data);
-            final File dumpFile = new File(dir, baseName + ".txt");
+                final byte[] data = Files.readAllBytes(file.toPath());
+                final String dump = Util.dumpData(data);
+                final File dumpFile = new File(dir, baseName + ".txt");
 
-            try (FileOutputStream os = new FileOutputStream(dumpFile)) {
-                os.write(dump.getBytes());
+                try (FileOutputStream os = new FileOutputStream(dumpFile)) {
+                    os.write(dump.getBytes());
+                }
+
+                final BaseModel model = HoTTDecoder.decodeFile(file);
+                final String html = HTMLReport.generateHTML(model);
+                final File htmlFile = new File(dir, baseName + ".html");
+                final File pdfFile = new File(dir, baseName + ".pdf");
+
+                HTMLReport.save(htmlFile, html);
+                PDFReport.save(pdfFile, html);
             }
-
-            final BaseModel model = HoTTDecoder.decodeFile(file);
-            final String html = HTMLReport.generateHTML(model);
-            final File htmlFile = new File(dir, baseName + ".html");
-            final File pdfFile = new File(dir, baseName + ".pdf");
-
-            HTMLReport.save(htmlFile, html);
-            PDFReport.save(pdfFile, html);
-        }
     }
 }
