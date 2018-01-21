be3d3a73cb6e4c4c35db4f8d60f0b13aa52440c8
Oliver Treichel <oli@treichels.de>
2018-01-21 13:41:50 +0100

    add support for format, codec and volume

--- HoTT-TTS/src/main/java/de/treichels/hott/tts/Text2SpeechService.kt
+++ HoTT-TTS/src/main/java/de/treichels/hott/tts/Text2SpeechService.kt
@@ -1,17 +1,32 @@
 package de.treichels.hott.tts
 
+import de.treichels.hott.model.voice.VolumeControlAudioInputStream
+import de.treichels.hott.tts.voicerss.Codec
+import de.treichels.hott.tts.voicerss.Format
 import de.treichels.hott.tts.voicerss.VoiceRSS
 import de.treichels.hott.tts.voicerss.VoiceRssLanguage
 import javafx.concurrent.Service
 import javafx.concurrent.Task
 import javax.sound.sampled.AudioInputStream
 
-class Text2SpeechService : Service<AudioInputStream>() {
+class Text2SpeechService() : Service<AudioInputStream>() {
     private val voiceRSS = VoiceRSS()
 
     override fun createTask(): Task<AudioInputStream> = object : Task<AudioInputStream>() {
-        override fun call(): AudioInputStream = voiceRSS.stream
+        override fun call(): AudioInputStream = VolumeControlAudioInputStream(voiceRSS.stream, volume)
     }
+
+    var codec: Codec
+        get() = voiceRSS.codec
+        set(codec) {
+            voiceRSS.codec = codec
+        }
+
+    var format: Format
+        get() = voiceRSS.format
+        set(format) {
+            voiceRSS.format = format
+        }
 
     var language: VoiceRssLanguage
         get() = voiceRSS.language
