00fa814adb00fbfbd5785fe4690c608144042b76
Oliver Treichel <oli@treichels.de>
2018-02-22 19:13:48 +0100

    New standalone tool to upgrade Slowflyer/Gyro receivers

---
+++ FirmwareUpgrader/src/main/java/de/treichels/hott/upgrade/FirmwareUpgradeService.kt
@@ -0,0 +1,82 @@
+package de.treichels.hott.upgrade
+
+import de.treichels.hott.model.HoTTException
+import de.treichels.hott.model.serial.SerialPort
+import javafx.concurrent.Service
+import javafx.concurrent.Task
+import tornadofx.*
+import java.io.File
+import java.nio.file.Files
+import java.util.*
+
+class FirmwareUpgradeService : Service<Unit>() {
+    lateinit var serialPort: SerialPort
+    lateinit var fileName: String
+    private var messages = ResourceBundle.getBundle(javaClass.name)
+
+    override fun createTask(): Task<Unit> = object : Task<Unit>() {
+        override fun call() {
+            val file = File(fileName)
+
+            if (!(file.exists() && file.isFile && file.canRead()))
+                throw NoSuchFileException(file)
+
+            val size = file.length()
+            val headerSize = 8
+            val blockSize = 0x406
+            val blockCount = (size.toInt() - headerSize) / blockSize
+            val bytes = Files.readAllBytes(file.toPath())
+
+            serialPort.use { port ->
+                port.timeout = 1000
+                port.open(19200)
+                val fileStream = bytes.inputStream()
+                val inputStream = port.inputStream
+                val outputStream = port.outputStream
+
+                //header
+                val type = fileStream.read()
+                fileStream.skip(7)
+
+                // wait for receiver
+                updateMessage(messages["waitForReceiver"])
+
+                do {
+                    try {
+                        val byte = inputStream.read()
+                        if (byte == type) break
+                    } catch (e: HoTTException) {
+                        if (isCancelled) return
+                    }
+                } while (true)
+
+                // write data
+                for (i in 1..blockCount) {
+                    updateProgress(i.toLong(), blockCount.toLong())
+                    updateMessage("${(i.toDouble() / blockCount.toDouble() * 100.0).toInt()} %")
+
+                    // read block from file
+                    val block = ByteArray(blockSize)
+                    fileStream.read(block)
+                    if (isCancelled) return
+
+                    // write block to receiver
+                    outputStream.write(block)
+                    outputStream.flush()
+                    if (isCancelled) return
+
+                    // read response
+                    val response = ByteArray(blockSize)
+                    inputStream.read(response)
+                    if (isCancelled) return
+
+                    // read status
+                    val byte = inputStream.read()
+
+                    if (!(response contentEquals block && byte == 0xaa)) throw HoTTException(messages["transmissionError"])
+                    if (isCancelled) return
+                }
+            }
+        }
+    }
+}
---
+++ FirmwareUpgrader/src/main/java/de/treichels/hott/upgrade/FirmwareUpgrader.kt
@@ -0,0 +1,226 @@
+package de.treichels.hott.upgrade
+
+import de.treichels.hott.model.serial.JSSCSerialPort
+import de.treichels.hott.model.serial.SerialPort
+import de.treichels.hott.util.ExceptionDialog
+import de.treichels.hott.util.MessageDialog
+import javafx.beans.property.SimpleObjectProperty
+import javafx.geometry.Pos
+import javafx.scene.control.*
+import javafx.scene.input.DragEvent
+import javafx.scene.input.TransferMode
+import javafx.scene.layout.Priority
+import javafx.stage.FileChooser
+import tornadofx.*
+import java.io.File
+
+fun main(vararg args: String) {
+    Thread.setDefaultUncaughtExceptionHandler { _, e -> ExceptionDialog.show(e) }
+    launch<FirmwareUpgraderApp>(*args)
+}
+
+class FirmwareUpgraderApp : App() {
+    override val primaryView = FirmwareUpgrader::class
+}
+
+class FirmwareUpgrader : View() {
+    companion object {
+        const val PREFERRED_PORT = "preferredPort"
+        const val LAST_DIR = "lastDir"
+    }
+
+    // resources
+    private val iconImage = resources.image("icon.png")
+
+    // controls
+    private var textField by singleAssign<TextField>()
+    private var portCombo by singleAssign<ComboBox<String>>()
+    private var progressBar by singleAssign<ProgressBar>()
+    private var progressLabel by singleAssign<Label>()
+    private var startButton by singleAssign<Button>()
+
+    // properties
+    private val serialPortProperty = SimpleObjectProperty<SerialPort?>(null)
+    private var serialPort by serialPortProperty
+
+    // background activity
+    private val service = FirmwareUpgradeService()
+
+    // UI
+    override val root = vbox {
+        spacing = 10.0
+        paddingAll = 5.0
+
+        hbox {
+            alignment = Pos.CENTER_LEFT
+            spacing = 5.0
+
+            label(messages["file"])
+            textField = textfield(messages["selectFile"]) {
+                hgrow = Priority.ALWAYS
+                disableWhen { service.runningProperty().or(textProperty().isEqualTo(messages["selectFile"])) }
+            }
+            button(messages["select"]) {
+                disableWhen { service.runningProperty() }
+                action { selectFile() }
+            }
+
+            region {
+                hgrow = Priority.SOMETIMES
+            }
+
+            label(messages["port"])
+            portCombo = combobox {
+                disableWhen { service.runningProperty() }
+                setOnAction {
+                    val portName = value
+
+                    if (portName != null) {
+                        preferences { put(PREFERRED_PORT, portName) }
+                        serialPort = JSSCSerialPort(portName)
+                    }
+                }
+            }
+        }
+
+        hbox {
+            alignment = Pos.CENTER_LEFT
+            spacing = 5.0
+
+            startButton = button(messages["start"]) {
+                disableWhen {
+                    serialPortProperty.isNull
+                            .or(textField.textProperty().isEmpty)
+                            .or(textField.textProperty().isEqualTo(messages["selectFile"]))
+                }
+                action {
+                    if (service.isRunning)
+                        doCancel()
+                    else
+                        doUpgrade()
+                }
+            }
+
+            anchorpane {
+                hgrow = Priority.ALWAYS
+
+                progressBar = progressbar {
+                    progress = 0.0
+                    anchorpaneConstraints {
+                        topAnchor = 0.0
+                        leftAnchor = 0.0
+                        rightAnchor = 0.0
+                        bottomAnchor = 0.0
+                    }
+                }
+
+                progressLabel = label {
+                    alignment = Pos.CENTER
+                    anchorpaneConstraints {
+                        topAnchor = 0.0
+                        leftAnchor = 0.0
+                        rightAnchor = 0.0
+                        bottomAnchor = 0.0
+                    }
+                }
+            }
+        }
+    }
+
+    private fun doCancel() {
+        service.cancel()
+        progressBar.progressProperty().unbind()
+        progressBar.progress = 0.0
+        progressLabel.textProperty().unbind()
+        progressLabel.text = null
+    }
+
+    private fun doUpgrade() {
+        progressBar.progressProperty().bind(service.progressProperty())
+        progressLabel.textProperty().bind(service.messageProperty())
+
+        startButton.text = messages["cancel"]
+
+        service.setOnFailed { ev ->
+            ExceptionDialog.show(ev.source.exception)
+            startButton.text = messages["start"]
+        }
+
+        service.setOnCancelled {
+            MessageDialog.show(Alert.AlertType.WARNING, messages["cancelled"], messages["upgradeCancelled"])
+            startButton.text = messages["start"]
+        }
+
+        service.setOnSucceeded {
+            MessageDialog.show(Alert.AlertType.INFORMATION, messages["success"], messages["upgradeFinished"])
+            startButton.text = messages["start"]
+        }
+
+        service.fileName = textField.text
+        service.serialPort = serialPort!!
+        service.restart()
+    }
+
+    private fun selectFile() {
+        FileChooser().apply {
+            title = messages["selectTitle"]
+            extensionFilters.add(FileChooser.ExtensionFilter(messages["firmwareFiles"], "*.bin"))
+
+            preferences {
+                val folder = File(get(LAST_DIR, System.getProperty("user.home")))
+                if (folder.exists() && folder.isDirectory)
+                    initialDirectory = folder
+            }
+        }.showOpenDialog(primaryStage)?.apply {
+            preferences { put(LAST_DIR, parentFile.absolutePath) }
+            textField.text = absolutePath
+        }
+    }
+
+    private fun isFirmware(file: File) = file.exists() && file.isFile && file.canRead() && file.name.endsWith(".bin")
+    private fun getFile(ev: DragEvent) = ev.dragboard.files.firstOrNull(::isFirmware)
+
+    init {
+        setStageIcon(iconImage)
+        title = messages["title"]
+
+        with(root) {
+            setOnDragEntered { ev ->
+                opacity = 0.3
+                ev.consume()
+            }
+
+            setOnDragExited { ev ->
+                opacity = 1.0
+                ev.consume()
+            }
+
+            setOnDragOver { ev ->
+                if (getFile(ev) != null) ev.acceptTransferModes(TransferMode.COPY)
+                ev.consume()
+            }
+
+            setOnDragDropped {ev->
+                val file = getFile(ev)
+                if (file != null) {
+                    textField.text = file.absolutePath
+                    preferences { put(LAST_DIR, file.parentFile.absolutePath) }
+                }
+                ev.consume()
+            }
+        }
+
+
+        // load preferred port from preferences
+        runAsync {
+            portCombo.items.addAll(JSSCSerialPort.availablePorts)
+        } success {
+            preferences {
+                val prefPort: String? = get(PREFERRED_PORT, null)
+                if (prefPort != null && portCombo.items.contains(prefPort)) {
+                    portCombo.value = prefPort
+                }
+            }
+        }
+    }
+}
