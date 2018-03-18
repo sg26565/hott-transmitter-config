2f8035716bb069f872193fecfcea4f2b7b1d0ff7
Oliver Treichel <oli@treichels.de>
2018-03-18 12:59:52 +0100

    convinience method to download all available firmware files

--- HoTT-Model/src/main/java/de/treichels/hott/model/firmware/Updatable.kt
+++ HoTT-Model/src/main/java/de/treichels/hott/model/firmware/Updatable.kt
@@ -3,4 +3,5 @@ package de.treichels.hott.model.firmware
 interface Updatable<T> where T : Updatable<T>, T : Enum<T> {
     val productCode: Int
     fun getFirmware(): List<Firmware<T>>
+    fun downloadFirmware() = getFirmware().forEach { it.download() }
 }
