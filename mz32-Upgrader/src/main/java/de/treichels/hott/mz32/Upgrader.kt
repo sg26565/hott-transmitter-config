9ec8e87b393cd99e49bcd3adfaedd82f109ad06e
Oliver Treichel <oli@treichels.de>
2018-05-21 09:32:23 +0200

    add tooltips

--- mz32-Upgrader/src/main/java/de/treichels/hott/mz32/Upgrader.kt
+++ mz32-Upgrader/src/main/java/de/treichels/hott/mz32/Upgrader.kt
@@ -3,6 +3,7 @@ package de.treichels.hott.mz32
 import de.treichels.hott.util.ExceptionDialog
 import javafx.concurrent.Task
 import javafx.geometry.Pos
+import javafx.scene.control.Button
 import javafx.scene.control.CheckBox
 import javafx.scene.control.ComboBox
 import javafx.scene.control.TextArea
@@ -13,7 +14,7 @@ import org.apache.logging.log4j.util.EnvironmentPropertySource
 import org.apache.logging.log4j.util.SystemPropertiesPropertySource
 import tornadofx.*
 
-private val ignored = listOf( LogFactoryImpl::class, EnvironmentPropertySource::class, SystemPropertiesPropertySource::class )
+private val ignored = listOf(LogFactoryImpl::class, EnvironmentPropertySource::class, SystemPropertiesPropertySource::class)
 
 fun main(vararg args: String) {
     Thread.setDefaultUncaughtExceptionHandler { _, e -> ExceptionDialog.show(e) }
@@ -28,9 +29,12 @@ class Mz32Upgrader : View() {
     // Controls
     private var reindex by singleAssign<CheckBox>()
     private var updateResources by singleAssign<CheckBox>()
+    private var replaceHelpPages by singleAssign<CheckBox>()
+    private var replaceVoiceFiles by singleAssign<CheckBox>()
     private var updateFirmware by singleAssign<CheckBox>()
     private var comboBox by singleAssign<ComboBox<Mz32>>()
     private var textArea by singleAssign<TextArea>()
+    private var updateButton by singleAssign<Button>()
 
     // Background task
     private var task: Task<Unit>? = null
@@ -43,6 +47,10 @@ class Mz32Upgrader : View() {
         get() = reindex.isSelected
     private val doUpdateResources
         get() = updateResources.isSelected
+    private val doReplaceHelpPages
+        get() = replaceHelpPages.isSelected
+    private val doReplaceVoiceFiles
+        get() = replaceVoiceFiles.isSelected
     private val doUpdateFirware
         get() = updateFirmware.isSelected
     private val mz32
@@ -61,12 +69,34 @@ class Mz32Upgrader : View() {
             vbox {
                 spacing = 5.0
 
-                reindex = checkbox("Re-index files on radio")
+                reindex = checkbox("Re-index files on radio") {
+                    tooltip = tooltip("Re-calculate the MD5 checksums of all files on the internal SD-card of the radio. This process will take a long time.")
+                }
+
                 updateResources = checkbox("Update resource files") {
                     isSelected = true
+                    tooltip = tooltip("Update auxilary resource files like help pages, voice files and utilities.")
                 }
+
+                vbox {
+                    padding = insets(left = 20.0)
+
+                    replaceHelpPages = checkbox("Replace custom help pages.") {
+                        enableWhen { updateResources.selectedProperty() }
+                        isSelected = true
+                        tooltip = tooltip("Updates help pages with the latest online version. WARNING! This will replace all custom help pages.")
+                    }
+
+                    replaceVoiceFiles = checkbox("Replace custom voice files.") {
+                        enableWhen { updateResources.selectedProperty() }
+                        isSelected = true
+                        tooltip = tooltip("Updates voice files with the latest online version. WARNING! This will replace all custom voice files.")
+                    }
+                }
+
                 updateFirmware = checkbox("Update firmware") {
                     isSelected = true
+                    tooltip = tooltip("Copies the latest firmware file to the internal SD-card. Installation has to be performed afterwards on the radio in the \"Info & Update\" menu via \"SD Card update\".")
                 }
             }
 
@@ -89,6 +119,7 @@ class Mz32Upgrader : View() {
         label("Messages:")
 
         textArea = textarea {
+            isEditable = false
             prefHeight = 200.0
             font = Font.font("Monospaced Regular", 12.0)
             vgrow = Priority.SOMETIMES
@@ -97,14 +128,16 @@ class Mz32Upgrader : View() {
         buttonbar {
             paddingAll = 5.0
 
-            button("Online Update") {
+            updateButton = button("Online Update") {
                 disableWhen { comboBox.valueProperty().isNull }
                 action {
                     textArea.text = ""
 
                     task = runAsync {
-                        if (doReindex) mz32.md5.scan(this)
-                        mz32.update(this, doUpdateResources, doUpdateFirware)
+                        if (doReindex) {
+                            mz32.scan(this)
+                        }
+                        mz32.update(this, doUpdateResources, doReplaceHelpPages, doReplaceVoiceFiles, doUpdateFirware)
                     }.apply {
                         disableWhen { runningProperty() }
                         messageProperty().addListener { _, _, newValue ->
@@ -127,5 +160,7 @@ class Mz32Upgrader : View() {
         primaryStage.setOnCloseRequest {
             task?.cancel()
         }
+
+        primaryStage.setOnShown { updateButton.requestFocus() }
     }
 }
