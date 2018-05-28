f3ac2ae436e17ecf394f639e6d918908707c8036
Oliver Treichel <oli@treichels.de>
2018-05-28 20:32:29 +0200

    move lzma code to a separate module

---
+++ lzma-sdk/src/main/java/de/treichels/lzma/LZMA.kt
@@ -0,0 +1,32 @@
+package de.treichels.lzma
+
+import SevenZip.Compression.LZMA.Decoder
+import SevenZip.Compression.LZMA.Encoder
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
+
+fun compress(inputStream: InputStream, outputStream: OutputStream) {
+    inputStream.use {
+        outputStream.use {
+            Encoder().apply {
+                SetEndMarkerMode(true)
+                WriteCoderProperties(outputStream)
+                Code(inputStream, outputStream, -1, -1, null)
+            }
+        }
+    }
+}
+
+fun uncompress(inputStream: InputStream, outputStream: OutputStream) {
+    inputStream.use {
+        outputStream.use {
+            Decoder().apply {
+                val properties = ByteArray(5)
+                if (inputStream.read(properties) != properties.size) throw IOException("input .lzma file is too short")
+                if (!SetDecoderProperties(properties)) throw IOException("Incorrect stream properties")
+                if (!Code(inputStream, outputStream, -1)) throw  IOException("Error in data stream")
+            }
+        }
+    }
+}
