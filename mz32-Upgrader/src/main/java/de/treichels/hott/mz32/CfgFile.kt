11b0e9923f13eec634446ad40d8d3573f1535d19
Oliver Treichel <oli@treichels.de>
2018-06-09 18:58:21 +0200

    ignore blank lines, comments and malformed lines

--- mz32-Upgrader/src/main/java/de/treichels/hott/mz32/CfgFile.kt
+++ mz32-Upgrader/src/main/java/de/treichels/hott/mz32/CfgFile.kt
@@ -3,6 +3,10 @@ package de.treichels.hott.mz32
 import java.io.File
 import java.util.*
 
+/**
+ * Parse a config file into a hash map. Each line consists of a key-value pair separated by a colon. Whitespace is
+ * ignored as well as empty lines and lines starting with #
+ */
 class CfgFile : HashMap<String, String>() {
     companion object {
         private const val delimiter = " : "
@@ -10,8 +14,10 @@ class CfgFile : HashMap<String, String>() {
         fun load(file: File) = CfgFile().apply {
             if (file.exists() && file.isFile && file.canRead()) {
                 println(file)
-                file.forEachLine { line ->
-                    if (line.contains(delimiter)) {
+                file.forEachLine { it ->
+                    val line = it.trim()
+
+                    if (!line.isBlank() && !line.startsWith("#") && line.contains(delimiter)) {
                         val name = line.substringBefore(delimiter).trim()
                         val value = line.substringAfter(delimiter).trim()
                         this[name] = value
