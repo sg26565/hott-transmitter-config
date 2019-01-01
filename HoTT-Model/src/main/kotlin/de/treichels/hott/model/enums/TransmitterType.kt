cd93cfcc3ce85667a6b1cd0c00c2c0e157f53423
Oliver Treichel <oli@treichels.de>
2019-01-01 17:19:56 +0100

    add order numbers for transmitters

--- HoTT-Model/src/main/kotlin/de/treichels/hott/model/enums/TransmitterType.kt
+++ HoTT-Model/src/main/kotlin/de/treichels/hott/model/enums/TransmitterType.kt
@@ -17,14 +17,14 @@ import java.util.*
 /**
  * @author Oliver Treichel &lt;oli@treichels.de&gt;
  */
-enum class TransmitterType(override val productCode: Int, override val orderNo: String = "", override val category: String = TransmitterType.category) : Registered<TransmitterType> {
-    mc16(16004600), mc20(16004300), mc26(16007700), mc28(16007100),
-    mc32(16004100), mc32Expert(16008500), mc32ExpertBeta(16008501),
-    mxs8(16004900), mx10(16004200), mx12(16003600), mx16(16003300),
-    mx20(16003700), mz10(16005000), mz12(16005100), mz12Pro(16007800),
-    mz16(16008600), mz16beta(16008601), mz18(16005300), mz18Pro(16008300),
-    mz24(16005200), mz24Pro(16007200), mz32(16008200), mz32beta(16008201),
-    x4s(16005900), x8n(16006200), x8e(16006500), Unknown(0);
+enum class TransmitterType(override val productCode: Int = 0, override val orderNo: String = "", override val category: String = TransmitterType.category) : Registered<TransmitterType> {
+    mc16(16004600, "33016"), mc20(16004300, "33020"), mc26(16007700, "S1036"), mc28(16007100, "33028"),
+    mc32(16004100, "33032"), mc32Expert(16008500), mc32ExpertBeta(16008501),
+    mxs8(16004900,"33200"), mx10(16004200, "33110"), mx12(16003600, "33112"), mx16(16003300, "33116"),
+    mx20(16003700,"33124"), mz10(16005000, "S1042"), mz12(16005100, "S1002"), mz12Pro(16007800, "S1002.PRO"),
+    mz16(16008600), mz16beta(16008601), mz18(16005300,"S1005"), mz18Pro(16008300, "S1005.PRO"),
+    mz24(16005200, "S1006"), mz24Pro(16007200, "S1006.PRO"), mz32(16008200, "S1024"), mz32beta(16008201, "S1024"),
+    x4s(16005900, "33400"), x8n(16006200, "S1018"), x8e(16006500, "S1008"), Unknown;
 
     override fun toString(): String = ResourceBundle.getBundle(javaClass.name)[name]
 
