48a072550f8eebc2f1d3189dac9bd697b78145f6
Dieter Baurcht <dieter.baurecht@gmx.at>
2019-01-12 16:45:17 +0100

    Funktionen für MZ-24PRO Decoder erweitert (Phasenschalter)

--- Test/src/main/kotlin/DecoderTest.kt
+++ Test/src/main/kotlin/DecoderTest.kt
@@ -1,21 +1,22 @@
-import de.treichels.hott.decoder.internal.decoder.BaseDecoder
-import de.treichels.hott.report.html.HTMLReport
+import de.treichels.hott.decoder.HoTTDecoder
+import de.treichels.hott.model.BaseModel
 import de.treichels.hott.report.xml.XMLReport
-import de.treichels.hott.util.Util
 import java.io.File
 
 fun main(vararg arg:String) {
     val path = "V:/MDL-Viewer_Daten/Models/mz-24pro"
     val filename = "hTEST HELI"
     val file = File(path,"$filename.mdl")
-    val model = BaseDecoder.decode(file)
+    val model: BaseModel = HoTTDecoder.decodeFile(file)
     val xmlOutput = XMLReport.generateXML(model)
     // val hex = Util.dumpData(file.readBytes())
     // println(hex)
     println(xmlOutput)
+/*
     val xmlFile = File(path,"$filename.xml")
     xmlFile.writeText(xmlOutput)
     val htmlFile = File(path,"$filename.html")
     val htmlOutput = HTMLReport.generateHTML(model)
     htmlFile.writeText(htmlOutput)
+*/
 }
