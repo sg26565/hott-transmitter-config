11077e210a8f670c30ef3e94295ba8cb6299c607
Oliver Treichel <oli@treichels.de>
2019-10-23 21:34:06 +0200

    add os specific dependency of openjfx-swing

--- MdlViewer/build.gradle.kts
+++ MdlViewer/build.gradle.kts
@@ -1,3 +1,17 @@
+import com.google.gradle.osdetector.OsDetector
+
+plugins {
+    application
+    osDetector
+}
+
+val os = OsDetector().os
+val os_platform = when (os) {
+    "osx" -> "mac"
+    "windows" -> "win"
+    else -> os
+}
+
 dependencies {
     implementation(project(":HoTT-Report-HTML"))
     implementation(project(":HoTT-Report-XML"))
@@ -5,11 +19,8 @@ dependencies {
     implementation(project(":HoTT-Serial"))
     implementation(Libs.commons_math3)
     implementation(Libs.javafx_swing)
+    implementation(Libs.javafx_swing + ":" + os_platform)
     runtimeOnly(project(":jSerialCommPort"))
-}
-
-plugins {
-    application
 }
 
 application {
