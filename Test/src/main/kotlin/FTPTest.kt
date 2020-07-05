ea2e47a9ebbe4a1f0eca492ddc527bd588caabfb
Oliver Treichel <oli@treichels.de>
2020-07-05 17:26:26 +0200

    measure timing

--- Test/src/main/kotlin/FTPTest.kt
+++ Test/src/main/kotlin/FTPTest.kt
@@ -1,6 +1,8 @@
 import de.treichels.hott.decoder.Firmware
-import de.treichels.hott.decoder.getFirmware
-import de.treichels.hott.model.enums.*
+import org.apache.commons.net.ftp.FTPClient
+import org.apache.commons.net.ftp.FTPReply
+import java.io.FileOutputStream
+import java.io.IOException
 
 fun Firmware<*>.print() {
     val cached = isCached
@@ -10,10 +12,37 @@ fun Firmware<*>.print() {
     if (!cached) download()
 }
 
-fun main(vararg args: String) {
-    ReceiverType.values().forEach { it.getFirmware().forEach(Firmware<*>::print) }
-    TransmitterType.values().forEach { it.getFirmware().forEach(Firmware<*>::print) }
-    ModuleType.values().forEach { it.getFirmware().forEach(Firmware<*>::print) }
-    SensorType.values().forEach { it.getFirmware().forEach(Firmware<*>::print) }
-    ESCType.values().forEach { it.getFirmware().forEach(Firmware<*>::print) }
+fun main() {
+//    ReceiverType.values().forEach { it.getFirmware().forEach(Firmware<*>::print) }
+//    TransmitterType.values().forEach { it.getFirmware().forEach(Firmware<*>::print) }
+//    ModuleType.values().forEach { it.getFirmware().forEach(Firmware<*>::print) }
+//    SensorType.values().forEach { it.getFirmware().forEach(Firmware<*>::print) }
+//    ESCType.values().forEach { it.getFirmware().forEach(Firmware<*>::print) }
+
+    FTPClient().apply {
+        try {
+            connect("ftp.graupner.de")
+
+            if (FTPReply.isPositiveCompletion(replyCode)) println("Connected to server") else throw IOException("cannot connect to server")
+
+            if (login("ftpuser01", "1fQjHTGc")) println("Login succeeded.") else throw IOException("Login failed")
+
+            val dir = "/Transmitter/16008200"
+            listFiles(dir).filter { it.isFile }.forEach {
+                val fileSize = it.size
+                val localFileName ="C:/Temp/${it.name}"
+                val remoteFileName = "$dir/${it.name}"
+                val start = System.currentTimeMillis()
+                val outputStream = FileOutputStream(localFileName)
+
+                print("Downloading $remoteFileName:$fileSize ... ")
+                if (!retrieveFile(remoteFileName,outputStream)) throw IOException("")
+                val duration = System.currentTimeMillis() - start
+                val rate = fileSize / 1024.0  * 1000.0 / duration
+                println ("took $duration ms (${rate.toInt()} kb/s)")
+            }
+        } finally {
+            if (isConnected) disconnect()
+        }
+    }
 }
