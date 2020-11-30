5a92fd7767ffa4a467f2ec5cabadef4849f5393f
Oliver Treichel <oli@treichels.de>
2020-11-30 21:57:18 +0100

    add Portuguese language

--- mz32-Downloader/src/main/kotlin/de/treichels/hott/mz32/Language.kt
+++ mz32-Downloader/src/main/kotlin/de/treichels/hott/mz32/Language.kt
@@ -4,7 +4,7 @@ import tornadofx.*
 import java.util.*
 
 enum class Language {
-    ch, cz, en, es, fr, ge, it, kr, nl;
+    ch, cz, en, es, fr, ge, it, kr, nl, pt;
 
     override fun toString(): String = ResourceBundle.getBundle(javaClass.name)[name]
 }
