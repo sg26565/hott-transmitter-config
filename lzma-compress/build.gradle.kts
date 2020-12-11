1eafae6388d8be967eee26f60fd91bad2eb929d6
Oliver Treichel <oli@treichels.de>
2020-12-11 12:00:53 +0100

    manage version for kotlinx.coroutines

--- lzma-compress/build.gradle.kts
+++ lzma-compress/build.gradle.kts
@@ -2,7 +2,7 @@ dependencies {
     implementation(project(":mz32-Downloader"))
     implementation(project(":HoTT-UI"))
     implementation(project(":lzma-sdk"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:_")
 }
 
 plugins {
