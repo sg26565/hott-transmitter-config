e8cdf86bb3de85944ef657e6751ac893787c0df0
klinwer <75271328+klinwer@users.noreply.github.com>
2021-02-18 21:35:52 +0100

    Prepare table view of models.

--- SaveModels/src/main/kotlin/de/treichels/hott/MyView.kt
+++ SaveModels/src/main/kotlin/de/treichels/hott/MyView.kt
@@ -1,19 +1,23 @@
 package de.treichels.hott
 
 import de.treichels.hott.decoder.HoTTTransmitter
+import de.treichels.hott.messages.Messages
 import de.treichels.hott.serial.SerialPort
+import de.treichels.hott.serial.ModelInfo
 import javafx.beans.property.SimpleStringProperty
 import javafx.collections.FXCollections
 import javafx.scene.paint.Color
 import tornadofx.*
 import java.time.LocalDate
 import java.time.Period
+import javax.swing.table.AbstractTableModel
 
 class MyView : View() {
 
     private val cities = FXCollections.observableArrayList("Dallas", "New York", "Sacramento")
-    private val selectedPort = SimpleStringProperty("COM?")
-    private val portList = FXCollections.observableArrayList("COM?")
+    private val dummyPort : String = "COM?"
+    private val selectedPort = SimpleStringProperty(dummyPort)
+    private val portList = FXCollections.observableArrayList(dummyPort)
 
     override val root = borderpane {
         top = form {
@@ -32,20 +36,11 @@ class MyView : View() {
             }
         }
 
-        center = tableview(persons) {
-            column("ID", Person::idProperty)
-            column("Name", Person::nameProperty)
-            column("Birthday", Person::birthdayProperty)
-            readonlyColumn("Age", Person::age).cellFormat {
-                text = it.toString()
-                style {
-                    if (it < 18) {
-                        backgroundColor += c("#8b0000")
-                        textFill = Color.WHITE
-                    }
-                }
-            }
-        }
+//        center = tableview(ModelInfoTableModel) {
+//            column(AbstractTableModel.)
+//
+//
+//        }
 
         bottom = hbox {
             button("Button 1") {
@@ -62,12 +57,17 @@ class MyView : View() {
         for ( s in SerialPort.getAvailablePorts()) {
             println("port: $s")
             portList.add(s)
-            portList.removeIf {portList -> portList == "COM?"}
+            portList.removeIf {portList -> portList == dummyPort}
         }
 
         selectedPort.onChange {
             println("Port changed to: $it" )
+            updateTableData(it.toString())
 
+            for (i in 0 until tableModel.rowCount){
+                val line = tableModel.getModelInfo(i)
+                println("$line")
+            }
         }
     }
 }
@@ -97,3 +97,69 @@ private val persons = FXCollections.observableArrayList(
     Person(3, "Stuart Gilles", LocalDate.of(1989, 5, 23)),
     Person(4, "Nicole Williams", LocalDate.of(1998, 8, 11))
 )
+
+private var port: HoTTTransmitter? = null
+private val tableModel =  ModelInfoTableModel()
+
+private class ModelInfoTableModel : AbstractTableModel() {
+    var modelInfos: Array<ModelInfo>? = null
+        private set
+
+    override fun getColumnCount(): Int {
+        return COLUMNS.size
+    }
+
+    override fun getColumnName(column: Int): String {
+        return COLUMNS[column]
+    }
+
+    fun getModelInfo(row: Int): ModelInfo {
+        return modelInfos!![row]
+    }
+
+    override fun getRowCount(): Int {
+        return if (modelInfos == null) 0 else modelInfos!!.size
+    }
+
+    override fun getValueAt(rowIndex: Int, columnIndex: Int): Any? {
+        val (_, modelName, modelInfo, modelType, _, receiverClass, usedHours, usedMinutes) = modelInfos!![rowIndex]
+
+        when (columnIndex) {
+            0 -> return rowIndex + 1
+
+            1 -> return modelType
+
+            2 -> return modelName
+
+            3 -> return modelInfo
+
+            4 -> return receiverClass
+
+            5 -> return if (usedHours == 0 && usedMinutes == 0) {
+                "-:-"
+            } else {
+                String.format("%02d:%02d", usedHours, usedMinutes)
+            }
+
+            else -> return null
+        }
+    }
+
+    override fun isCellEditable(rowIndex: Int, columnIndex: Int): Boolean {
+        return columnIndex == 6
+    }
+
+    fun setModelInfos(modelInfos: List<ModelInfo>) {
+        this.modelInfos = modelInfos.toTypedArray()
+        fireTableDataChanged()
+    }
+
+    companion object {
+        private val COLUMNS = arrayOf(Messages.getString("ModelNumber"), Messages.getString("ModelType"), Messages.getString("ModelName"), Messages.getString("ModelInfo"), Messages.getString("ReceiverType"), Messages.getString("Usage"), Messages.getString("Actions"))
+    }
+}
+
+private fun updateTableData(portName: String) {
+    port = HoTTTransmitter(SerialPort.getPort(portName))
+    tableModel.setModelInfos(port!!.allModelInfos)
+}
