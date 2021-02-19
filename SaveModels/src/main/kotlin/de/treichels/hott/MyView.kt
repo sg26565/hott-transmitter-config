e2e792affb599c7ac3efaff7920b0c5ad6b18d57
klinwer <Klingenbeck@t-online.de>
2021-02-19 16:02:45 +0100

    New table view added, first tests of integration.

--- SaveModels/src/main/kotlin/de/treichels/hott/MyView.kt
+++ SaveModels/src/main/kotlin/de/treichels/hott/MyView.kt
@@ -4,8 +4,11 @@ import de.treichels.hott.decoder.HoTTTransmitter
 import de.treichels.hott.messages.Messages
 import de.treichels.hott.serial.SerialPort
 import de.treichels.hott.serial.ModelInfo
+import javafx.beans.property.SimpleIntegerProperty
 import javafx.beans.property.SimpleStringProperty
 import javafx.collections.FXCollections
+import javafx.scene.control.Button
+import javafx.scene.control.TableCell
 import tornadofx.*
 import javax.swing.table.AbstractTableModel
 
@@ -15,8 +18,17 @@ class MyView : View() {
     private val selectedPort = SimpleStringProperty(dummyPort)
     private val portList = FXCollections.observableArrayList(dummyPort)
 
+    private val warriorModel : WarriorModel by inject()
+    private val persons = FXCollections.observableArrayList(
+        Warrior(1,"Tyrion Lannister", "M"),
+        Warrior(2,"Ned Stark", "M"),
+        Warrior(3,"Daenerys Targaryen", "F"),
+        Warrior(4,"Arya Stark", "F")
+    )
+
     override val root = borderpane {
-        top = form {
+        top {
+            form {
             fieldset {
                 field("Select Port") {
                     combobox(selectedPort, portList)
@@ -30,17 +42,36 @@ class MyView : View() {
                 }
             }
         }
+        }
 
-
-
-        bottom = hbox {
-            button("Button 1") {
-                hboxConstraints {
-                    marginRight = 20.0
-
+        center {
+            tableview(persons) {
+                isEditable = true
+                column("ID", Warrior::idProperty)
+                column("Name", Warrior::nameProperty).makeEditable()
+                column("Gender", Warrior::genderProperty).useComboBox(FXCollections.observableArrayList("M", "F"))
+                column("Action", Warrior::dummyProperty).setCellFactory {DeleteButton<Warrior>()}
+                column("Action", Warrior::dummyProperty).setCellFactory {AddButton<Warrior>()}
+                bindSelected(warriorModel)
+                subscribe<DeleteEvent> { event ->
+                    items.removeAt(event.index)
+                }
+                subscribe<AddEvent> {
+                    items.add(Warrior(persons.size+1,"hugo","F"))
                 }
             }
-            button("Button 2")
+        }
+
+        bottom {
+            hbox {
+                button("Button 1") {
+                    hboxConstraints {
+                        marginRight = 20.0
+
+                    }
+                }
+                button("Button 2")
+            }
         }
     }
 
@@ -128,3 +159,47 @@ private fun updateTableData(portName: String) {
     port = HoTTTransmitter(SerialPort.getPort(portName))
     tableModel.setModelInfos(port!!.allModelInfos)
 }
+
+class DeleteButton<Warrior> : TableCell<Warrior, String?>() {
+    private val btn = Button("Delete")
+    override fun updateItem(item: String?, empty: Boolean) {
+        super.updateItem(item, empty)
+        if (empty) {
+            graphic = null
+            text = null
+        } else {
+            btn.setOnAction {
+                //tableView.items.removeAt(index)
+                FX.eventbus.fire(DeleteEvent(index))
+            }
+            graphic = btn
+            text = null
+        }
+    }
+}
+class AddButton<Warrior> : TableCell<Warrior, String?>(){
+    private val btn = Button("Add")
+    override fun updateItem(item: String?, empty: Boolean) {
+        super.updateItem(item, empty)
+        if (empty){
+            graphic = null
+            text = null
+        } else{
+            btn.setOnAction {
+                FX.eventbus.fire(AddEvent(index))
+            }
+            graphic = btn
+            text = null
+        }
+    }
+}
+class Warrior(id: Int, name: String, gender: String) {
+
+    val idProperty = SimpleIntegerProperty(id)
+    val nameProperty = SimpleStringProperty(name)
+    val genderProperty = SimpleStringProperty(gender)
+    val dummyProperty = SimpleStringProperty("")
+}
+class WarriorModel : ItemViewModel<Warrior>()
+class AddEvent(val index:Int) : FXEvent()
+class DeleteEvent(val index: Int) : FXEvent()
